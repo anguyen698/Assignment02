85b6516548f1e38c1ef6a455261d66023006ec70
anguyen698 <63482191+anguyen698@users.noreply.github.com>
2026-02-24 19:48:10 -0800

    Update 02-word-translator.js messages

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,11 +1,12 @@
 let languageCode = prompt('Select a language code ("es", "de", "en", and "fr")').toLowerCase()
 
 if (languageCode === "es") {
-    window.console.log("Hello World translated in Spanish is: Hola Mundo")
+    window.console.log("Hello World in Spanish is Hola Mundo")
 } else if (languageCode === "de") {
-    window.console.log("Hello World translated in German is: Hallo Welt")
+    window.console.log("Hello World in German is Hallo Welt")
 } else if (languageCode === "fr") {
-    window.console.log("Hello World translated in French is: Bonjour le Monde")
+    window.console.log("Hello World in French is Bonjour le Monde")
 } else {
-    window.console.log("Hello World translated in English is: Hello World")
+    window.console.log("Hello World in English is Hello World")
 }
+
